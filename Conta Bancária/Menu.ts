5d890eb8b824747694c12f72658f8676ca440986
dani-furtado <danicfurtado@outlook.com>
2024-03-26 17:54:33 -0300

    Criando classe Conta

--- Conta Bancária/Menu.ts
+++ Conta Bancária/Menu.ts
@@ -1,20 +1,31 @@
 import readlinesync = require("readline-sync");
+import { colors } from './node_modules/src/util/colors';
+import { Conta } from './node_modules/src/model/Conta';
 
 export function main () {
 
     let menu: number;
 
-    while (true){
-        console.log("Bem vindo ao Banco DCF. Escolha a opção do menu abaixo:")
-        console.log("\nOpção 1: Criar conta")
-        console.log("Opção 2: Listar todas as contas")
-        console.log("Opção 3: Buscar conta por número")
-        console.log("Opção 4: Atualizar dados da conta")
-        console.log("Opção 5: Apagar conta")
-        console.log("Opção 6: Sacar")
-        console.log("Opção 7: Depositar")
-        console.log("Opção 8: Transferir")
-        console.log("Opção 9: Sair")
+    let c1: Conta = new Conta(1, 123, 1, "Natasha", 1000000)
+    c1.visualizar()
+
+
+    while (true) {
+
+        console.log(colors.bg.black, colors.fg.yellow, 
+
+        console.log("Bem vindo ao Banco DCF. Escolha a opção do menu abaixo:");
+        console.log("\nOpção 1: Criar conta");
+        console.log("Opção 2: Listar todas as contas");
+        console.log("Opção 3: Buscar conta por número");
+        console.log("Opção 4: Atualizar dados da conta");
+        console.log("Opção 5: Apagar conta");
+        console.log("Opção 6: Sacar");
+        console.log("Opção 7: Depositar");
+        console.log("Opção 8: Transferir");
+        console.log("Opção 9: Sair");
+        
+        colors.reset);
 
         console.log("\nQual opção você deseja prosseguir?")
         menu = readlinesync.questionInt("")
