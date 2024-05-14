3256b82940787ece954e7d59ac5cdd958a84f568
dani-furtado <danicfurtado@outlook.com>
2024-05-14 20:24:32 -0300

    Adicionando evento DOM

---
+++ Bloco 3/Exercício 1/script.js
@@ -0,0 +1,7 @@
+var botao = document.getElementById("botao2");
+
+// Adicionando um evento de clique ao botão
+botao.addEventListener("click", function() {
+  // Seu código a ser executado quando o botão for clicado
+  window.alert("want to see?");
+});
